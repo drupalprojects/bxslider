540b0e7a7a7688cfac974c9890e03989f0768dc2
VVVi <VVVi@355825.no-reply.drupal.org>
2015-01-19 21:20:27 +0200

    Issue #2370561 by mrmikedewolf: Can't add multiple sliders to same page

--- bxslider.tpl.php
+++ bxslider.tpl.php
@@ -10,7 +10,7 @@
       <li><?php print $item['slide']; ?></li>
     <?php endforeach; ?>
   </ul>
-  <?php if($settings['slider_settings']['pagerCustom_type'] == 'thumbnail_pager_method1'): ?>
+  <?php if(isset($settings['slider_settings']['pagerCustom_type']) && $settings['slider_settings']['pagerCustom_type'] == 'thumbnail_pager_method1'): ?>
     <div id="<?php  print $settings['slider_settings']['pagerCustom'] ?>">
       <?php foreach($items as $key => $item): ?>
         <a data-slide-index="<?php print $key ?>" href=""><?php print $item['slide_pagerCustom']; ?></a>
