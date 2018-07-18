ceb412efcc5dee7e544c7912aa56ee11762d3e00
Victor Vilyus <VVVi@355825.no-reply.drupal.org>
2018-07-18 23:13:02 +0300

    Update, local js according to improvements done for d7 version. Fix codding standard. Fix an error when image style wasn't selected be default.

--- js/bxslider.js
+++ js/bxslider.js
@@ -8,11 +8,28 @@
       for (var slider_id in settings.bxslider) {
 //        $('#' + slider_id, context).once('bxslider-' + slider_id, function () {
 
-          if (settings.bxslider[slider_id].buildPager) {
-              settings.bxslider[slider_id].buildPager = new Function('slideIndex', settings.bxslider.buildPager);
-              settings.bxslider[slider_id].pagerCustom = null;
-          }
-          $('#' + slider_id + ' .bxslider', context).show().bxSlider(settings.bxslider[slider_id]);
+        var slider_settings = settings.bxslider[slider_id];
+
+        if (slider_settings.buildPager) {
+          slider_settings.buildPager = new Function('slideIndex', slider_settings.buildPager);
+          slider_settings.pagerCustom = null;
+        }
+
+        var indexCorrection = 0;
+        if(slider_settings.infiniteLoop) {
+          // If enabled Infinite Loop, then slide index increased on 1;
+          indexCorrection = 1;
+        }
+
+        slider_settings.onSlideAfter = function($slideElement, oldIndex, newIndex){
+          $(this).find('li.active-slide').removeClass("active-slide");
+          $(this).find('li').eq(newIndex + indexCorrection).addClass("active-slide");
+        }
+
+        var slider = $('#' + slider_id + ' .bxslider', context).show().bxSlider(slider_settings);
+
+        var current = slider.getCurrentSlide();
+        slider.find('li').eq(current + indexCorrection).addClass("active-slide");
 
 //        });
       }
