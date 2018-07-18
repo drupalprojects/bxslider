f37dd31b2df1814f15a58b6747de3c9a8fce398c
V <VVVi@355825.no-reply.drupal.org>
2018-07-18 18:33:52 +0300

    Issue #2372397 by Se7enT1, OFF, VVVi: Add class to active slide

--- js/bxslider.js
+++ js/bxslider.js
@@ -6,14 +6,31 @@
         return;
       }
       for (var slider_id in settings.bxslider) {
+
         $('#' + slider_id, context).once('bxslider-' + slider_id, function () {
 
-          if (settings.bxslider[slider_id].slider_settings.buildPager) {
-              settings.bxslider[slider_id].slider_settings.buildPager = new Function('slideIndex', settings.bxslider.slider_settings.buildPager);
-              settings.bxslider[slider_id].slider_settings.pagerCustom = null;
-          }
-          $('#' + slider_id + ' .bxslider', context).show().bxSlider(settings.bxslider[slider_id].slider_settings);
+          var slider_settings = settings.bxslider[slider_id].slider_settings;
 
+          if (slider_settings.buildPager) {
+            slider_settings.buildPager = new Function('slideIndex', slider_settings.buildPager);
+            slider_settings.pagerCustom = null;
+          }
+
+          var indexCorrection = 0;
+          if(slider_settings.infiniteLoop) {
+            // If enabled Infinite Loop, then slide index increased on 1;
+            indexCorrection = 1;
+          }
+
+          slider_settings.onSlideAfter = function($slideElement, oldIndex, newIndex){
+            $(this).find('li.active-slide').removeClass("active-slide");
+            $(this).find('li').eq(newIndex + indexCorrection).addClass("active-slide");
+          }
+
+          var slider = $('#' + slider_id + ' .bxslider', context).show().bxSlider(slider_settings);
+
+          var current = slider.getCurrentSlide();
+          slider.find('li').eq(current + indexCorrection).addClass("active-slide");
         });
       }
     }
